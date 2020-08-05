13b7cc48667813be73994a348b91e47a12901fad
Joaquim Castro <joaquimcastro2909@gmail.com>
2020-08-05 08:00:09 -0300

    finished

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 const inputs = document.querySelectorAll(".controls input");
 
-const handleUpdate = () => {
-  console.log(this.value);
-};
+function handleUpdate() {
+  const sufix = this.dataset.sizing || "";
+
+  document.documentElement.style.setProperty(`--${this.name}`, this.value + sufix);
+}
 
 inputs.forEach( input => input.addEventListener("change", handleUpdate));
+inputs.forEach( input => input.addEventListener("mousemove", handleUpdate));
